3c8c8467b10f42038bb8602522b91721d105f2a6
Evgeniy Kvach <Evgeniy.Kvach@newdv.ru>
2019-11-28 09:41:52 +0300

    update todo

--- ebike.config.js
+++ ebike.config.js
@@ -1,20 +1,21 @@
 const target = 'http://192.168.2.101:4444';
 module.exports = {
-	aliases: {
-		'101': 'http://192.168.2.101:4444',
-	},
-	exeptions: ['git', 'js_babeled', 'node_modules', 'build', 'hotreload', 'idea', 'package.json', 'scripts'], // исключения,которые вотчить не надо, файлы и папки
-	clientOptions: {
-		port: '9080',
-	},
-	isRemoteServer: true,
-	remoteServerOptions: {
-		path: '/myServer',
-		proxy: {
-			target,
-			changeOrigin: true,
-			ws: true,
-		},
-	},
-	pathToWatch: './',
-}
+  entry: 'index.html', // located in root project directory
+  aliases: {
+    101: 'http://192.168.2.101:4444',
+  },
+  exeptions: ['git', 'js_babeled', 'node_modules', 'build', 'hotreload', 'idea', 'package.json', 'scripts'], // исключения,которые вотчить не надо, файлы и папки
+  clientOptions: {
+    port: '9080',
+  },
+  isRemoteServer: true,
+  remoteServerOptions: {
+    path: '/myServer',
+    proxy: {
+      target,
+      changeOrigin: true,
+      ws: true,
+    },
+  },
+  pathToWatch: './',
+};
